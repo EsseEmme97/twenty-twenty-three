67f991dbd69cb084d8a4c336e0f286376238ea0a
Sebastian <sebastian9705@gmail.com>
2024-08-25 16:48:54 +0200

    added staggering effects to Cards

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,19 +1,31 @@
+"use client"
+import { motion } from "framer-motion"
 import Image from "next/image"
 
 interface CardProps {
-	title: String,
-	content:String,
-	date:String
-
+  title: String,
+  content: String,
+  date: String
 }
 
-export default function Card({title,content,date}:CardProps){
-	return(
-		<div className="p-2">
-			<Image src="https://picsum.photos/300/200?grayscale" width={300} height={200} className="min-w-full" alt="imagePlaceholder"/>
-			<h2 className="text-6xl mt-8 hover:underline">{title}</h2>
-			<p className="my-8">{content}</p>
-			<p>{date}</p>
-		</div>
-	)
+const item = {
+  hidden: { opacity: 0, y: 20 },
+  show: { opacity: 1, y: 0 }
+}
+
+export default function Card({ title, content, date }: CardProps) {
+  return (
+    <motion.div className="p-2" variants={item}>
+      <Image
+        src="https://picsum.photos/300/200?grayscale"
+        width={300}
+        height={200}
+        className="min-w-full"
+        alt="imagePlaceholder"
+      />
+      <h2 className="text-6xl mt-8 hover:underline">{title}</h2>
+      <p className="my-8">{content}</p>
+      <p>{date}</p>
+    </motion.div>
+  )
 }
--- src/app/components/ImagesGrid.tsx
+++ src/app/components/ImagesGrid.tsx
@@ -1,12 +1,35 @@
+"use client"
 import Card from "./Card"
 import data from "../../../mock/dummyData"
+import { motion } from "framer-motion"
 
-export default function ImagesGrid(){
-	console.log(data);
-	return(
-		<div className="grid grid-cols-3 gap-4">
-			{data.map(element=><Card title={element.title} content={element.content} date={element.date}/>)}
-			
-		</div>
-	)
+export default function ImagesGrid() {
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+		duration:1,
+        staggerChildren: 0.3
+      }
+    }
+  }
+
+  return (
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="show"
+      className="grid grid-cols-3 gap-4"
+    >
+      {data.map((element, index) => (
+        <Card
+          key={index}
+          title={element.title}
+          content={element.content}
+          date={element.date}
+        />
+      ))}
+    </motion.div>
+  )
 }
